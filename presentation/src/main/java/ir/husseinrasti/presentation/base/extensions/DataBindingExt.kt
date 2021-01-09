484f8897b212acbc9ad470ed5189c955209496ec
Hussein Rasti <husseinrasti@gmail.com>
2021-01-09 03:42:30 +0330

    Init vibration and play music and change image tag

--- presentation/src/main/java/ir/husseinrasti/presentation/base/extensions/DataBindingExt.kt
+++ presentation/src/main/java/ir/husseinrasti/presentation/base/extensions/DataBindingExt.kt
@@ -3,6 +3,7 @@ package ir.husseinrasti.presentation.base.extensions
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import ir.husseinrasti.presentation.R
 
 @BindingAdapter("app:imageUrl")
 fun loadImage(view: ImageView, url: String?) = view.apply {
@@ -14,5 +15,20 @@ fun loadImage(view: ImageView, url: String?) = view.apply {
             .load(url)
             .placeholder(circularProgressDrawable())
             .into(this)
+    }
+}
+
+@BindingAdapter("app:setImage")
+fun setImage(view: ImageView, code: Int) = view.apply {
+    when (code) {
+        0 -> {
+            setImageResource(R.drawable.ic_image)
+        }
+        1 -> {
+            setImageResource(R.drawable.ic_vibrate)
+        }
+        2 -> {
+            setImageResource(R.drawable.ic_sound)
+        }
     }
 }
--- presentation/src/main/java/ir/husseinrasti/presentation/card/CardFragment.kt
+++ presentation/src/main/java/ir/husseinrasti/presentation/card/CardFragment.kt
@@ -1,25 +1,36 @@
 package ir.husseinrasti.presentation.card
 
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import ir.husseinrasti.domain.card.entity.Card
 import ir.husseinrasti.presentation.R
 import ir.husseinrasti.presentation.base.BaseFragment
 import ir.husseinrasti.presentation.base.ResultError
-import ir.husseinrasti.presentation.base.extensions.*
+import ir.husseinrasti.presentation.base.extensions.observeResult
 import ir.husseinrasti.presentation.databinding.FragmentCardBinding
-import kotlinx.android.synthetic.main.fragment_card.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class CardFragment : BaseFragment() {
 
     private val viewModel: CardViewModel by viewModels()
 
     private lateinit var binding: FragmentCardBinding
+
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +52,7 @@ class CardFragment : BaseFragment() {
     private fun setupViewModel() {
         viewModel.randomCard.observeResult(this, onSuccess = {
             binding.card = it
+            doWork(it)
         }, onError = {
             binding.error = ResultError(
                 message = it?.message ?: getString(R.string.error_an_error_occur_try_again),
@@ -48,6 +60,46 @@ class CardFragment : BaseFragment() {
                 show = it?.show ?: true
             )
         })
+    }
+
+    private fun doWork(card: Card?) = runBlocking(Dispatchers.IO) {
+        stopSound()
+        when (card?.code) {
+            1 -> {//vibrate
+                val vibrate = requireContext().getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrate.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+                } else {
+                    vibrate.vibrate(500)
+                }
+            }
+            2 -> {//sound
+                playSound(card.sound)
+            }
+            else -> { //Nothing
+            }
+        }
+    }
+
+    private fun playSound(sound: String?) = runBlocking(Dispatchers.IO) {
+        try {
+            mediaPlayer = MediaPlayer().apply {
+                setDataSource(sound)
+                prepare() // might take long! (for buffering, etc)
+                start()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun stopSound() {
+        try {
+            mediaPlayer?.release()
+            mediaPlayer = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
