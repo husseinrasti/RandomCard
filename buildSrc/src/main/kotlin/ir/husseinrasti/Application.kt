e2af0a1b6603c1617eef96aabba05102c1804895
Hussein Rasti <husseinrasti@gmail.com>
2021-09-16 00:21:40 +0430

    Fix action with name

--- buildSrc/src/main/kotlin/ir/husseinrasti/Application.kt
+++ buildSrc/src/main/kotlin/ir/husseinrasti/Application.kt
@@ -2,8 +2,8 @@ package dependencies
 
 object Application {
     const val id = "ir.husseinrasti.randomcard"
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 31
+    const val versionName = "1.0.1.$versionCode"
 
     const val baseUrl = "https://my-json-server.typicode.com/husseinrasti/randomcard/"
 }
